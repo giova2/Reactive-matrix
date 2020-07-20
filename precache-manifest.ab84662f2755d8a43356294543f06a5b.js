dd1d678c648c1ae9a4de5805113e60adc97cc684
franco giovannangelo <fcgiova@gmail.com>
2020-07-20 18:04:55 -0300

    Updates

---
+++ precache-manifest.ab84662f2755d8a43356294543f06a5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e346490dacf6c23cdd43",
+    "url": "/Reactive-matrix/static/css/main.695abc26.chunk.css"
+  },
+  {
+    "revision": "e346490dacf6c23cdd43",
+    "url": "/Reactive-matrix/static/js/main.2f817ffb.chunk.js"
+  },
+  {
+    "revision": "4046b7302a6e65390826",
+    "url": "/Reactive-matrix/static/js/runtime~main.b9da82c9.js"
+  },
+  {
+    "revision": "f5470452aec0bd61a447",
+    "url": "/Reactive-matrix/static/js/2.4cbfd1a3.chunk.js"
+  },
+  {
+    "revision": "e7420c41af33a760e35e8fafbf92127a",
+    "url": "/Reactive-matrix/index.html"
+  }
+];
